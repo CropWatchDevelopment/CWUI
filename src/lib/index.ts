c7c8f80dad03c174bc9a684d6285b8a14bdd36a5
Kevin Cantrell <kevin@cropwatch.io>
2025-04-13 15:14:57 +0900

    exporting datalist

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 // Reexport your entry components here
 export { default as DashboardCard } from './components/DashboardCard.svelte';
 export { default as DataRowItem } from './components/DataRowItem.svelte';
+export { default as DeviceDataList } from './components/DeviceDataList.svelte';
+
 
 // Export interfaces
 export type { 
